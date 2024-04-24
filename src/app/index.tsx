a1718e4501dbc089a4ffaac8cda06ef24887dca4
Michal Vavrek <vavrek@duxsoft.sk>
2024-04-24 20:21:42 +0200

    home screen: polls list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
+
+const polls = [1, 2, 3];
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello a World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Stack.Screen options={{title: 'Polls'}} />
+    <FlatList
+      data={polls}
+      contentContainerStyle={styles.container}
+      renderItem={() => (
+        <View style={styles.pollContainer}>
+          <Text style={styles.pollTitle}>Hlasujem za moznost</Text>
+        </View>
+      )}
+    />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    gap: 5,
+  },
+  pollContainer: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  pollTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
